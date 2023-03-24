Fix geary gain computation

veca/metrics.py
@@ -92,7 +92,7 @@ def morans_i(x: np.ndarray, adj: sp.spmatrix) -> np.ndarray:
     return N / W * n / (d + 1e-16)
 
 
-def gearys_c(x: np.ndarray, adj: sp.spmatrix, adjustment: float = 1e-8) -> np.ndarray:
+def gearys_c(x: np.ndarray, adj: sp.spmatrix, adjustment: float = 1e-16) -> np.ndarray:
     """Compute the Geary's C spatial autocorrelation statistic.
 
     Parameters
@@ -125,6 +125,9 @@ def gearys_c(x: np.ndarray, adj: sp.spmatrix, adjustment: float = 1e-8) -> np.nd
     d = np.sum((x - np.mean(x, axis=0)) ** 2, axis=0)
 
     score = (N - 1) / (2 * W) * n / (d + 1e-16)
+    # The adjustment can make Geary's C a really large number, so we clamp that
+    # to 1, which is the maximal possible value.
+    score = np.minimum(score, 1)
 
     if score.size == 1:  # if a single attribute is passed, the size is (1,)
         score = score[0]

veca/preprocessing.py
@@ -251,13 +251,18 @@ def merge_overfragmented(
         v1_purity_gain = new_variable.purity / v1.purity - 1
         v2_purity_gain = new_variable.purity / v2.purity - 1
         purity_gain = np.mean([v1_purity_gain, v2_purity_gain])
-        geary_gain = (
-            min(v1.gearys_c, v2.gearys_c) / (new_variable.gearys_c + 1e-8) - 1
-        )
+
+        v1_geary_gain = (1 - new_variable.gearys_c) / (1 - v1.gearys_c + 1e-16)
+        v2_geary_gain = (1 - new_variable.gearys_c) / (1 - v2.gearys_c + 1e-16)
+        # geary_gain = (
+        #     min(v1.gearys_c, v2.gearys_c) / (new_variable.gearys_c + 1e-16) - 1
+        # )
+        geary_gain = np.mean([v1_geary_gain, v2_geary_gain])
+        # geary_gain += 1e-8  # undo ronuding error in geary gain
 
         return int(
             purity_gain >= min_purity_gain
-            and geary_gain >= min_geary_gain
+            and geary_gain >= min_geary_gain - 1e-4
             and shared_sample_pct >= min_sample_overlap
         )
 