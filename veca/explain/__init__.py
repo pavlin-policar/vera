ac568d6dc24afe11a2d90c0e98a1091970d2a32e
Pavlin Poličar <pavlin.g.p@gmail.com>
2024-02-22 14:23:42 +0100

    Move filtering from explain to descriptive

--- veca/explain/__init__.py
+++ veca/explain/__init__.py
@@ -1,19 +1,2 @@
-from veca.variables import ExplanatoryVariable
-
 from .contrastive import contrastive
-from .descriptive import descriptive
-
-
-def filter_explanatory_features(
-    variables: list[ExplanatoryVariable],
-    min_samples: int = 5,
-    min_purity: float = 0.5,
-    max_geary_index: float = 1,  # no filtering by Geary
-):
-    return [
-        v
-        for v in variables
-        if v.purity >= min_purity
-        and v.gearys_c <= max_geary_index
-        and v.num_contained_samples >= min_samples
-    ]
+from .descriptive import descriptive, filter_explanatory_features
--- veca/explain/descriptive.py
+++ veca/explain/descriptive.py
@@ -5,7 +5,6 @@ import numpy as np
 from scipy import stats as stats
 
 from veca import metrics as metrics, graph as g
-from veca.explain import filter_explanatory_features
 from veca.variables import ExplanatoryVariable, ExplanatoryVariableGroup, Variable
 
 
@@ -177,3 +176,18 @@ def descriptive(
         return layouts, filtered_clusters
     else:
         return layouts
+
+
+def filter_explanatory_features(
+    variables: list[ExplanatoryVariable],
+    min_samples: int = 5,
+    min_purity: float = 0.5,
+    max_geary_index: float = 1,  # no filtering by Geary
+):
+    return [
+        v
+        for v in variables
+        if v.purity >= min_purity
+        and v.gearys_c <= max_geary_index
+        and v.num_contained_samples >= min_samples
+    ]
